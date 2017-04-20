9c94f4aa34ffff7541f40ca48ba380c2b9f0fa1c
Andrew Aziz <andrew.aziz@swace.se>
2017-04-20 16:06:20 +0200

    requests

--- requests.js
+++ requests.js
@@ -12,31 +12,38 @@ module.exports = {
 		self.io = require('socket.io')(server);
 		var io = this.io;
 		io.on('connection', function (socket) {
-			socket.on('event:test', function (data) {
-				console.log("test event",data);
+			socket.on('event:getAutocomple', function (data) {
+				autocompleteStats(socket, data);
 			});
-			requestExample(socket);
-
+			socket.on('event:getStats', function(data) {
+				brandStats(socket, data);
+			});
+			autocompleteStats(socket, "hm");
 		});
 	}
 }
 
-function requestExample(socket) {
-	console.log("requestExample");
+function brandStats(socket, brand) {
 	var options = {
-		url: 'https://api.plick.se/api/v2/brands/by_slug/hm/statistics.json'
+		url: 'https://api.plick.se/api/v2/brands/by_slug/' + brand +  '/statistics.json'
 	};
 	request(options, function(error, response, body) {
 		if (!error && response.statusCode == 200) {
-			console.log(body);
-			socket.emit("event:hmstats", body);
+			socket.emit("event:returnStats", JSON.parse(body));
 		}
 	});
 }
 
-function callback(error, response, body) {
-	console.log("resp: ", response.statusCode);
-	if (!error && response.statusCode == 200) {
-		console.log(body);
-	}
+function autocompleteStats(socket, brand) {
+	console.log('https://api.plick.se/api/v2/brands/autocomplete');
+	var options = {
+		url: 'https://api.plick.se/api/v2/brands/autocomplete',
+		qs: {query: brand}
+	};
+	request(options, function(error, response, body) {
+		console.log(response);
+		if (!error && response.statusCode == 200) {
+			socket.emit("event:returnAutocomplete", JSON.parse(body));
+		}
+	});
 }
